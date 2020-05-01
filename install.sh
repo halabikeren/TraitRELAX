044c76b12cd3ca36c6c2e80679754ac6f35f8e1d
halabikeren <halabikeren@gmail.com>
2020-05-01 14:39:41 +0300

    directories access corrections

--- install.sh
+++ install.sh
@@ -8,45 +8,50 @@ traitrelax_dir=TRAITRELAX_INSTALLATION_DIRECTORY
 
 ## install Bio++
 mkdir -p $bpp_dir/sources/
+cd $bpp_dir/sources
+git clone https://github.com/BioPP/bpp-core.git
+git clone https://github.com/BioPP/bpp-seq.git
+git clone https://github.com/BioPP/bpp-phyl.git
+git clone https://github.com/BioPP/bpp-popgen.git
 cd $bpp_dir
 git clone https://github.com/BioPP/bppsuite.git
-cd sources
-git clone https://github.com/BioPP/bpp-core.git
+cd $bpp_dir/sources/bpp-core
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$bpp_dir -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE ..
-make
+make -j
 make install
-git clone https://github.com/BioPP/bpp-seq.git
+cd ../../bpp-seq
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$bpp_dir -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE ..
-make
+make -j
 make install
-git clone https://github.com/BioPP/bpp-phyl.git
+cd ../../bpp-phyl
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$bpp_dir -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE ..
-make
+make -j
 make install
-git clone https://github.com/BioPP/bpp-popgen.git
+cd ../../bpp-popgen
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$bpp_dir -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE ..
-make
+make -j
 make install
-cd ../../../bppsuite/
+cd ../../../bppsuite
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$bpp_dir -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE ..
-make
+make -j
 make install
 
 ## install traitrelax
 mkdir -p $traitrelax_dir
 cd $traitrelax_dir
 git clone https://github.com/halabikeren/traitrelax.git
+cd TraitRELAX
 cmake -DCMAKE_INSTALL_PREFIX=$bpp_dir -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE .
-make
+make -j
 make install
 
